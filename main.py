898cef90d95e2397509a94257b0b9314c13f621b
werter <arslanagica504@gmail.com>
2024-01-25 11:43:22 +0100

    in Program instance all classes in constructor write template for status led update requirements.txt

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ from audio_interface import AudioInterface
 from translater import Translater
 
 class Program:
-    def main(self):
-        audio_interface: AudioInterface = AudioInterface()
-        audio_interface.speak("audios/warning.mp3")
 
-        tinyllam: TinyLlama11b = TinyLlama11b()
-        print(tinyllam.send("why is the sky blue"))
+    def __init__(self):
+        self.tinyllama: TinyLlama11b = TinyLlama11b()
+        # TODO: pins
+        self.status_led: LED = LED()
+        self.button: Button = Button(1)
+        self.audio: AudioInterface = AudioInterface()
+        self.translater: Translater = Translater()
+
+    def main(self):
+        print("test")
+
+        self.audio.speak("audios/warning.mp3")
+
+       # print(self.tinyllama.send("why is the sky blue"))
 
 
 
--- status_led.py
+++ status_led.py
@@ -1,14 +1,51 @@
 
 class LED:
 
-    def __init__(self, gpioPin_R, gpioPin_G, gpioPin_B):
-        self.gpioPin_R = gpioPin_R
-        self.gpioPin_G = gpioPin_G
-        self.gpioPin_B = gpioPin_B
+    def __init__(self, gpioPin_R: int = 1, gpioPin_G: int = 1, gpioPin_B: int = 1):
+        self.gpioPin_R: int = gpioPin_R
+        self.gpioPin_G: int = gpioPin_G
+        self.gpioPin_B: int = gpioPin_B
 
-    def on(self, r, g, b):
-        # Wert den es annehmen soll oder an oder aus ??
+    def off(self):
         raise NotImplementedError
 
-    def of(self):
+    def white(self):
+        #r on
+        #g on
+        #b on
+        raise NotImplementedError
+
+    def red(self):
+        #r on
+        #g off
+        #b off
+        raise NotImplementedError
+
+    def green(self):
+        # r off
+        # g on
+        # b off
+        raise NotImplementedError
+
+    def blue(self):
+        # r off
+        # g off
+        # b on
+        raise NotImplementedError
+
+    def yellow(self):
+        # r on
+        # g on
+        # b off
+        raise NotImplementedError
+
+    def lightblue(self):
+        # r off
+        # g on
+        # b off
+        raise NotImplementedError
+    def magenta(self):
+        # r on
+        # g off
+        # b on
         raise NotImplementedError
